5e07abbbf02c9105c40c913a063c678c04395652
DESKTOP-6S2ESAI\Valentine <vamone13@hotmail.com>
2020-07-25 22:08:55 +0100

    Step motors changed steps.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -749,7 +749,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+ //Valentine
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 92.94 } //(old)Z4000
 
 /**
  * Default Max Feed Rate (mm/s)
